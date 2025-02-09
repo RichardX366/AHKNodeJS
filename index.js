35eafc72101d4e397f4586371209695a6cd56e01
Arkadiusz Czekajski <aczekajski@gmail.com>
2025-02-09 17:25:25 +0900

    remove raw flag

--- index.js
+++ index.js
@@ -356,7 +356,7 @@ module.exports = async function (path, hotkeysList, options) {
     /**
      * Types out a string. Look at documentation for extra information.
      * @param {'send' | 'sendInput' | 'sendPlay'} sendType - type of "send" command
-     * @param {{ msg: string, blind?: boolean, raw?: boolean} | string} x - The string to send
+     * @param {{ msg: string, blind?: boolean } | string} x - The string to send
      */
     async sendGeneric(sendType, x) {
       if (typeof x === 'string') x = { msg: x };
@@ -375,21 +375,21 @@ module.exports = async function (path, hotkeysList, options) {
     },
     /**
      * Types out a string. Look at documentation for extra information.
-     * @param {{ msg: string, blind?: boolean, raw?: boolean} | string} x - The string to send
+     * @param {{ msg: string, blind?: boolean } | string} x - The string to send
      */
     async send(x) {
       return ahk.sendGeneric('send', x);
     },
     /**
      * Types out a string using SendInput. Look at documentation for extra information.
-     * @param {{ msg: string, blind?: boolean, raw?: boolean} | string} x - The string to send
+     * @param {{ msg: string, blind?: boolean } | string} x - The string to send
      */
     async sendInput(x) {
       return ahk.sendGeneric('sendInput', x);
     },
     /**
      * Types out a string using SendPlay. Look at documentation for extra information.
-     * @param {{ msg: string, blind?: boolean, raw?: boolean} | string} x - The string to send
+     * @param {{ msg: string, blind?: boolean } | string} x - The string to send
      */
     async sendPlay(x) {
       return ahk.sendGeneric('sendPlay', x);
